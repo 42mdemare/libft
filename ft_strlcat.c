1a31814cacb2c0f421babc03a3681e8de56f6705
KaliStudio <kalicem76@hotmail.fr>
2024-10-21 22:21:50 +0200

    Update ft_strlcat.c suppr cast inutile + simplification boucle while

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,26 +14,23 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char	*s;
 	size_t	dstlen;
 	size_t	srclen;
 	size_t	i;
 	size_t	result;
 
-	s = (char *)src;
 	dstlen = ft_strlen(dst);
-	srclen = ft_strlen(s);
+	srclen = ft_strlen(src);
 	i = 0;
 	if (size > dstlen)
 		result = srclen + dstlen;
 	else
 		result = srclen + size;
-	while (s[i] && (dstlen + 1) < size)
+	while (src[i] && (dstlen + i) < size - 1)
 	{
-		dst[dstlen] = s[i];
-		dstlen++;
+		dst[dstlen + i] = src[i];
 		i++;
 	}
-	dst[dstlen] = '\0';
+	dst[dstlen + i] = '\0';
 	return (result);
 }
